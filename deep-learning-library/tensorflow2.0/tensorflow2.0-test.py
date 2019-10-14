d0c707307e8932d0fac1186817f59e9a7e5a23d1
cgump <824592076@qq.com>
2019-10-14 18:33:46 +0800

    tensorflow2.0开始

---
+++ deep-learning-library/tensorflow2.0/tensorflow2.0-test.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+print(tf.__version__)
+# tf.enable_eager_execution()
+
+X = tf.constant([[1., 2.], [3., 4.]])
+y = tf.constant([[1.], [2.]])
+w = tf.Variable(initial_value=[[1.], [2.]])
+b = tf.Variable(initial_value=1.)
+with tf.GradientTape() as tape:
+    L = 0.5 * tf.reduce_sum(tf.square(tf.matmul(X, w) + b - y))
+w_grad, b_grad = tape.gradient(L, [w, b])        # 计算L(w, b)关于w, b的偏导数
+print([L.numpy(), w_grad.numpy(), b_grad.numpy()])
