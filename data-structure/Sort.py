7b1e8f73fcacfc7c6f83b0e0412597d5441288c9
CGump <44838713+CGump@users.noreply.github.com>
2020-06-25 23:03:55 +0800

    sort agthom

---
+++ data-structure/Sort.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# ================================================================
+#
+#   Editor      : PyCharm
+#   File name   : Sort.py
+#   Author      : CGump
+#   Email       : huangzhigang93@qq.com
+#   Created date: 2020/6/25 22:46
+#
+# ================================================================
+
+
+def quick_sort(inputs: list):
+    if len(inputs) < 2:
+        return inputs
+    keys = inputs.pop(0)
+    less = []
+    greater = []
+    for item in inputs:
+        if item < keys:
+            less.append(item)
+        else:
+            greater.append(item)
+    return quick_sort(less) + [keys] + quick_sort(greater)
+
+
+def bubble_sort(inputs: list):
+    lengths = len(inputs)
+    while lengths > 0:
+        for i in range(0, len(inputs) - 1):
+            if inputs[i] > inputs[i+1]:
+                inputs[i], inputs[i+1] = inputs[i+1], inputs[i]
+        lengths -= 1
+    return inputs
+
+
+if __name__ == '__main__':
+    rst = quick_sort([3, 5, 1, 2, 4])
+    print(rst)
+    rst = bubble_sort([3, 5, 1, 2, 4])
+    print(rst)
